03fe71bcc9ceff7647f475546aebfc6fa84eeeea
Spencer Ueda <iwamuraspencer@gmail.com>
2017-03-13 11:07:17 +0000

    edit files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
 
   def show
-    @user = User.find(params[:id]).page(params[:page]).per(5)
+    @user = User.find(params[:id])
     @microposts = @user.microposts.order(created_at: :desc)
   end
   
